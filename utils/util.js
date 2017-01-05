2efc7e01d5efc8941a01fef7c823c154aa4e32c8
liujians <870891018@qq.com>
2017-01-05 15:26:47 +0800

    fetch修改为普通wx.request

--- utils/util.js
+++ utils/util.js
@@ -36,68 +36,68 @@ function getUser(obj){
     obj.setData({
             hidden: false
         })
-    fetch(app.globalData.ip+'/getUser.php')  
-    .then(  
-        function(response) {  
-            if (response.status !== 200) {  
-                console.log('错误码: ' +  
-                response.status);  
-                return;  
-            }
-            console.log(response)
-            // Examine the text in the response  
-            response.json().then(function(data) {  
-                console.log(data);
+    // fetch(app.globalData.ip+'/getUser.php')  
+    // .then(  
+    //     function(response) {  
+    //         if (response.status !== 200) {  
+    //             console.log('错误码: ' +  
+    //             response.status);  
+    //             return;  
+    //         }
+    //         console.log(response)
+    //         // Examine the text in the response  
+    //         response.json().then(function(data) {  
+    //             console.log(data);
+    //             setTimeout(function(){
+    //                 obj.setData({
+    //                     list:data,
+    //                     hidden: true,
+    //                     toast1Hidden:false,
+    //                     toastText:"拿到数据"
+    //                 })
+    //                 wx.stopPullDownRefresh()
+    //             },3000) 
+    //         });  
+    //     }  
+    // )  
+    // .catch(function(err) {  
+    //     obj.setData({
+    //         hidden: true
+    //     })
+    //     console.log('Fetch Error :-S', err);  
+    // });
+
+   wx.request({
+            url: app.globalData.ip+'/getUser.php',
+            data: {},
+            header: {
+                'Content-Type': 'application/json'
+            },
+            success: function(res) {
                 setTimeout(function(){
                     obj.setData({
-                        list:data,
+                        list:res.data,
                         hidden: true,
                         toast1Hidden:false,
                         toastText:"拿到数据"
                     })
                     wx.stopPullDownRefresh()
-                },3000) 
-            });  
-        }  
-    )  
-    .catch(function(err) {  
-        obj.setData({
-            hidden: true
-        })
-        console.log('Fetch Error :-S', err);  
-    });
-
-//    wx.request({
-        //     url: app.globalData.ip+'/getUser.php',
-        //     data: {},
-        //     header: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     success: function(res) {
-        //         setTimeout(function(){
-        //             obj.setData({
-        //                 list:res.data,
-        //                 hidden: true,
-        //                 toast1Hidden:false,
-        //                 toastText:"拿到数据"
-        //             })
-        //             wx.stopPullDownRefresh()
-        //         },3000)
+                },3000)
                  
-        //     },
-        //     fail:function(err){
-        //         setTimeout(function(){
-        //             obj.setData({
-        //                 list:res.data,
-        //                 hidden: true,
-        //                 toast1Hidden:false,
-        //                 toastText:"请检查server"
-        //             })
-        //             wx.stopPullDownRefresh()
-        //         },3000)
-        //         console.log(err);
-        //     }
-        // })
+            },
+            fail:function(err){
+                setTimeout(function(){
+                    obj.setData({
+                        list:res.data,
+                        hidden: true,
+                        toast1Hidden:false,
+                        toastText:"请检查server"
+                    })
+                    wx.stopPullDownRefresh()
+                },3000)
+                console.log(err);
+            }
+        })
 }
 function getMoments(obj){
     wx.request({
